d650c2e59a6bd9db218457ba7da5ba73b78538f9
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-05-14 18:41:13 -0300

    Criando Model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+      postId: {
+        foreignKey: true,
+        type: DataTypes.INTEGER,
+      },
+      categoryId: {
+        foreignKey: true,
+        type: DataTypes.INTEGER,
+      }
+    }, {
+        timestamps: false,
+        tableName: 'posts_categories',
+        underscored: true,
+    });
+  
+    PostCategory.associate = (models) => {
+      models.Category.belongsToMany(models.BlogPost, {
+        as: 'postId',
+        through: PostCategory,
+        foreignKey: 'categoryId',
+        otherKey: 'postId',
+      });
+      models.BlogPost.belongsToMany(models.Category, {
+        as: 'categories',
+        through: PostCategory,
+        foreignKey: 'postId',
+        otherKey: 'categoryId',
+      })
+    };
+  
+    return PostCategory;
+  };
