73c3eec3014a670c307d6e22796032a9add0cab1
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-05-14 16:44:13 -0300

    Criando Model User

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+module.exports= (sequelize, DataTypes) => {
+    const User= sequelize.define('User', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+          },
+          displayName: {
+            type: DataTypes.STRING,
+          },
+          email: {
+            allowNull: false,
+            type: DataTypes.STRING,
+          },
+          password: {
+            allowNull: false,
+            type: DataTypes.STRING,
+          },
+          image: {
+            allowNull: false,
+            type: DataTypes.STRING,
+          },
+        }, {
+          sequelize,
+          tableName: 'users',
+          timestamps: false,
+          underscored: true,
+        });
+      
+        User.associate = (models) => {
+          User.hasMany(models.BlogPost, {
+            foreignKey: 'user_id', as: 'blogPost',
+          })
+        }
+      
+        return User;
+      };
